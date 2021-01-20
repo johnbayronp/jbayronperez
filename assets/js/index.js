95db1ac2336a4f3fc026ca4e7ffa2eadc43beabb
root <webdeprueba1.1@gmail.com>
2021-01-20 18:54:57 -0500

    letras animadas

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+
+var typed = new Typed("#typed", {
+    strings:["Portafolio <Strong>Casas inteligentes.</Strong>",
+    "Diseño de <Strong>Iluminación.</Strong>",
+    "Alexa, <Strong> Apagar luz de estudio.</Strong>",
+    "Alexa, <Strong> Abrir garage</Strong>."],
+    
+    backDelay: 1000,
+    startDelay: 50,
+    smartBackspace: true, 
+    loop: true
+  });
+  
+  
+var typed2 = new Typed("#typed2", {
+    strings:["Diseño <Strong>Logos Empresariales.</Strong>",
+    "Diseño <Strong>Marca Personal.</Strong>",
+    "Diseño <Strong> App Moviles</Strong>."],
+    
+    backDelay: 1500,
+    smartBackspace: true, 
+    loop: true
+});
